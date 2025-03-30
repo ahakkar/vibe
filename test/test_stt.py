52242de8381066dcf66a5fd0d32f9fa10827c1b3
Totti Kuusimurto <totti.kuusimurto@protonmail.com>
2025-03-30 09:59:55 +0300

    broken the build

--- test/test_stt.py
+++ test/test_stt.py
@@ -1,7 +1,11 @@
 import pytest
 import os
-import numpy as np  
+import numpy as np
+import onnxruntime as ort
+from pathlib import Path  
 from unittest.mock import patch, MagicMock
+from transformers import Wav2Vec2Processor
+
 
 # Mock the imports of the modules that are not installed
 with patch.dict(
@@ -9,8 +13,6 @@ with patch.dict(
     {
         "torch": MagicMock(),
         "torchaudio": MagicMock(),
-        "onnxruntime": MagicMock(),
-        "transformers": MagicMock(),
     },
 ):
     from src.backend.local.stt import SpeechToTextService
@@ -21,25 +23,18 @@ class TestSTTService:
         """
         Setup method to run before each test
         """
-        self.project_root = "/path/to/project/root"
-        with patch.dict(
-            os.environ,
-            {
-                "MODEL_FOLDER": "test_model_folder",
-                "PROCESSOR": "test_processor",
-                "ONNX_MODEL": "test_onnx_model",
-            },
-        ):
-            self.stt_service = SpeechToTextService(self.project_root)
+        self.project_root = Path(__file__).resolve()
+        print(self.project_root)
+        print(os.getenv("MODEL_FOLDER"))
+        print(os.getenv("PROCESSOR"))
+        print(os.getenv("ONNX_MODEL"))
+        self.stt_service = SpeechToTextService(self.project_root)
 
     def teardown_method(self):
         """
         Teardown method to run after each test
         """
         self.stt_service = None
-        os.environ.pop("MODEL_FOLDER", None)
-        os.environ.pop("PROCESSOR", None)
-        os.environ.pop("ONNX_MODEL", None)
         self.project_root = None
 
     @pytest.mark.unit()
@@ -47,11 +42,13 @@ class TestSTTService:
         """
         Test the try block of initialization of the STT class
         Assert that the processor and ort_session are not None
-        """
+        """      
         assert self.stt_service.processor is not None
         assert self.stt_service.ort_session is not None
+        print(self.stt_service.processor.config.name_or_path)
+        print(self.stt_service.ort_session.get_model_path())
 
-    @pytest.mark.skip()
+    @pytest.mark.unit()
     def test_speech_to_text_service_init_except(self):
         """
         Test the except block of initialization of the STT class
@@ -59,9 +56,12 @@ class TestSTTService:
         """
         with patch.object(
             Wav2Vec2Processor, "from_pretrained", side_effect=Exception("Error")
-            ), patch.object(ort, "InferenceSession", side_effect=Exception("Error")):
-            assert self.stt_service.processor is not None
-            assert self.stt_service.ort_session is not None
+            ), patch.object(ort, "InferenceSession", side_effect=Exception("Error")
+            ), patch("builtins.print") as mock_print:
+            mock_print.assert_any_call(
+                "Failed to wav2vec processor: test_model_folder/test_processor\nError")
+            mock_print.assert_any_call(
+                "Failed to wav2vec onnx file: test_model_folder/test_onnx_model\nError")
 
 
     @pytest.mark.skip()
@@ -75,11 +75,11 @@ class TestSTTService:
         audio_data = np.random.randn(16000).astype(np.float32)
 
         with patch.object(
-            stt_service.ort_session, "run", return_value=[np.random.randn(1, 100, 32)]
+            self.stt_service.ort_session, "run", return_value=[np.random.randn(1, 100, 32)]
         ) as mock_run, patch.object(
-            stt_service.processor, "batch_decode", return_value=["test transcription"]
+            self.stt_service.processor, "batch_decode", return_value=["test transcription"]
         ) as mock_decode:
-            result = stt_service.process_audio(audio_data)
+            result = self.stt_service.process_audio(audio_data)
             mock_run.assert_called_once()
             mock_decode.assert_called_once()
             assert result == "test transcription"
