d1a6158445ad800a5e421afc88a26f419433c5dd
Antti Hakkarainen <a.hakkarainen@iki.fi>
2025-04-01 12:49:23 +0300

    test stubs for weather and yle services

---
+++ test/test_yle.py
@@ -0,0 +1,17 @@
+import pytest
+
+from pathlib import Path
+from unittest.mock import MagicMock, patch
+from src.backend.local.yle import YleNewsApi
+
+class TestYleNewsApi:
+    def setup_method(self):
+        """Setup method to run before each test."""
+        self.app = MagicMock()
+        self.root = Path(__file__).parent.parent / "src" / "backend"
+        with patch("pathlib.Path.__new__", return_value=self.root):
+            self.ir_service = Weather(self.app)
+
+    def teardown_method(self):
+        """ """
+        pass
