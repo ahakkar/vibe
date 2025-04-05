c575a15f17991c9245e5a88a2c3656a2655087ab
Veikka Liukkonen <veikka.liukkonen@tuni.fi>
2025-04-05 15:33:29 +0300

    Create yle page class

--- src/backend/local/yle.py
+++ src/backend/local/yle.py
@@ -7,9 +7,80 @@ import os
 from local.constants import TTV_PAGES
 from local.baseform import Baseform
 
+#For quick testing
+#from baseform import Baseform
+#from constants import TTV_PAGES
 
 # TODO: Remove page numbers from returned strings
 # TODO: Create a way to access articles based on the page numbers
+
+class YlePage:
+
+    def __init__(self):
+        self.subpages = {}
+        self.content = []
+
+    def _add_subpage(self, title: str, page_number: int):
+
+        self.subpages[title] = page_number
+
+    def _add_content(self, line: str):
+
+        self.content.append(line)
+
+    def get_titles(self):
+        
+        return_list = []
+        for title in self.subpages:
+            return_list.append(title)
+
+    def get_content(self):
+        return self.content
+    
+    #TODO: Improve to use finnwordnet?
+    def _find_page_from_input(self, input: str):
+        """
+        Finds the most similiar article title to user input by simply comparing words
+        Returns None if fails to find a related article
+
+        :param str input: The user input
+        :return int: The page number for the most similar article
+        """
+
+
+        b = Baseform()
+
+        input_words = input.lower().split()
+        input_baseforms = set(b.get_baseform(word) for word in input_words)
+
+        most_similiar = None
+        most_common_words = 0
+
+
+        for title in self.subpages:
+            
+            title_words = title.lower().split()
+            title_baseforms = set(b.get_baseform(word) for word in title_words)
+
+            intersection = input_baseforms & title_baseforms
+
+            if len(intersection) > most_common_words:
+                most_similiar = title
+                most_common_words = len(intersection)
+
+        if most_similiar is not None:
+            return self.subpages.get(most_similiar)
+        
+        else:
+            print("Failed to find article")
+            return None
+    
+        
+            
+
+
+
+
 
 
 class YleNewsApi:
@@ -52,7 +123,7 @@ class YleNewsApi:
 
         b = Baseform()
 
-        words = input.split()
+        words = input.lower().split()
 
         return_list = []
 
@@ -85,6 +156,7 @@ class YleNewsApi:
             return ["Uutisten hakeminen epäonnistui."]
 
         else:
+            
             return self._parse_json(page_data)
 
     def _get_page_data(self, page_number: int = 100) -> Optional[Dict[str, Any]]:
@@ -109,16 +181,28 @@ class YleNewsApi:
         Print the json data
 
         :param json json_data: The data that needs to be printed
+        :return YlePage: returns yle page object
         """
         teletext = json_data["teletext"]["page"]["subpage"][0]
         content = teletext["content"][0]
         lines = content["line"]
 
-        return_list = []
+        page = YlePage()
+        
+
 
         for line in lines:
             for _, value in line.items():
                 if len(value) > 2:
-                    return_list.append(value)
+                    
+                    words = value.split()
 
-        return return_list
+                    if words[0].isdigit():
+                        title = ' '.join(words[1:])                        
+                        page._add_subpage(title=title, page_number=int(words[0]))
+
+                    else:
+                        page._add_content(line = value)
+
+        return page
+    
