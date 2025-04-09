4fe1d554294579ea22a9b253df12398e579496c0
Masood <masood.ahmadi@tuni.fi>
2025-04-09 12:57:55 +0300

    frontend structure is changed

---
+++ frontend/app/__init__.py
@@ -0,0 +1,11 @@
+# app/__init__.py
+
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    from app.route import main_bp
+    app.register_blueprint(main_bp)
+
+    return app
