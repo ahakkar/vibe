c6514c45087ace7e71809e3618eb0823fa0c42cc
vboxuser <vboxuser@user.myguest.virtualbox.org>
2025-04-11 12:39:59 +0300

    change the templates structure

--- frontend/app/route.py
+++ frontend/app/route.py
@@ -8,9 +8,21 @@ main_bp = Blueprint('main', __name__)
 def index():
     return render_template('dashboard.html')
 
+@main_bp.route('/tg')
+def tg():
+    return render_template('tg.html')
+
+@main_bp.route('/stt')
+def stt():
+    return render_template('stt.html')
+
 @main_bp.route('/tts')
 def tts():
     return render_template('tts.html')
+
+@main_bp.route('/all')
+def all():
+    return render_template('chat.html')
 
 @main_bp.route('/tts/chat')
 def chat():
