ea01c69e26f4c02c44bfd47ab306880048512b28
Veikka Liukkonen <veikka.liukkonen@tuni.fi>
2025-04-27 14:17:08 +0300

    Add some yle unit tests

---
+++ test/yle_test_consts.py
@@ -0,0 +1,8 @@
+TEST_NEWS_LIST = ["PÄÄUUTINEN 1",
+                  "PÄÄUUTINEN 2",
+                  "PÄÄUUTINEN 3",
+                  "PÄÄUUTINEN 4"]
+
+TEST_FAIL_LIST = ["Anteeksi, en ymmärtänyt mitä uutisia haluat kuulla.",
+                  "Mistä aiheesta haluat kuulla uutisia: pääuutiset, kotimaa, ulkomaat, talous vai urheilu?"]
+
